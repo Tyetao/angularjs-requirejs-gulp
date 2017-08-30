58c5a72756214dc6e211aa1d3e4693cf2d54a23f
tanyetao <765068439@qq.com>
2017-08-30 09:14:09 +0800

    很多

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -2,10 +2,9 @@ define([
   'app/app',
   'css!/assets/css/login/login'
 ], function (app) {
-
   app.controller('loginCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.login = function () {
-      console.log()
+      console.log($scope.password)
     };
   }]);
 
